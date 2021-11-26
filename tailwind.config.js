1201571ef46fc17be07600cfc46ab90d1f9b08f7
Nathan Boiron <nathan.boiron@gmail.com>
2021-11-26 22:09:00 +0100

    Add a dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,19 @@ module.exports = {
     purge: [
         './public/**/*.html',
     ],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
         colors: {
             white: colors.white,
             black: colors.black,
-            gray: colors.blueGray,
+            gray: colors.coolGray,
             orange: colors.orange,
             blue: colors.blue,
+            logo: {
+                800: '#563130',
+                500: '#9c6333',
+                300: '#cc9a60',
+            }
         },
     },
     variants: {
